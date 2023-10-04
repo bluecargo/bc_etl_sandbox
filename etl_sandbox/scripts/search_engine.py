92ab1afad0c9177d5678f10f629a433731439180
Pierre Pasquet <80340281+ppasquet@users.noreply.github.com>
2023-10-03 21:56:55 -0400

    sync

---
+++ etl_sandbox/scripts/search_engine.py
@@ -0,0 +1,117 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+
+spark = SparkSession.builder.config("spark.driver.memory", "8g").appName('SparkByExamples.com').getOrCreate()
+
+spark.read.parquet("data/source-tables/scraping_scrapingsession/*.parquet").createOrReplaceTempView("sessions")
+spark.read.parquet("data/source-tables/search_transactionrefreshschedule/*.parquet").createOrReplaceTempView("schedules")
+spark.read.parquet("data/source-tables/containers_shipment_transactions/*.parquet").createOrReplaceTempView("shipment_transactions")
+
+spark.sql(
+    """
+    SELECT 
+        id
+        , created_at
+        , context
+        , status
+        , CASE
+            WHEN terminal_id IS NOT NULL THEN 'terminal'
+            ELSE 'shipping_line'
+        END AS target_type
+        , COALESCE(terminal_id, shipping_line_id) AS target_id
+        , CASE 
+            WHEN transaction_id IS NOT NULL THEN 'transaction'
+            ELSE 'shipment'
+        END AS entity_type
+        , COALESCE(transaction_id, shipment_id) AS entity_id
+    FROM sessions
+    """
+).createOrReplaceTempView("sessions")
+
+spark.sql(
+    """
+    SELECT
+        s.created_at
+        , s.id
+        , s.context
+        , s.status
+        , s.target_id
+        , entity_id AS transaction_id
+        , NULL AS derived_from_shipment
+    FROM sessions s
+    WHERE entity_type = 'transaction'
+    """
+).createOrReplaceTempView("transaction_sessions")
+
+spark.sql(
+    """
+    SELECT 
+        s.created_at
+        , s.id 
+        , s.context
+        , s.status
+        , s.target_id
+        , st.containertransaction_id AS transaction_id
+        , s.entity_id AS derived_from_shipment
+    FROM sessions s
+    LEFT JOIN shipment_transactions st 
+        ON st.shipment_id = s.entity_id
+    WHERE s.entity_type = 'shipment'
+    """
+).createOrReplaceTempView("shipment_sessions")
+
+spark.sql(
+    """
+    SELECT t.* FROM transaction_sessions t
+    UNION ALL 
+    SELECT s.* FROM shipment_sessions s
+    """
+).createOrReplaceTempView("sessions")
+
+spark.sql(
+    """
+    SELECT
+        sche.transaction_id
+        , sche.target_type
+        , sche.target_id
+        , sche.created_at
+        , sche.terminated_at
+        , sche.termination_code
+        , sche.context
+        , sche.id AS schedule_id
+        , s.id AS session_id
+        , s.created_at AS session_created_at
+        , s.status AS session_status
+        , s.context AS session_context
+        , s.target_id AS session_target_id
+        , CASE 
+            WHEN s.created_at < sche.created_at THEN 'early'
+            WHEN s.created_at > COALESCE(sche.terminated_at, sche._export_timestamp) THEN 'late'
+            WHEN s.created_at BETWEEN sche.created_at AND COALESCE(sche.terminated_at, sche._export_timestamp) THEN 'in_scope'
+        END AS punctuality
+    FROM schedules sche
+    LEFT JOIN sessions s
+        ON (s.transaction_id = sche.transaction_id AND UPPER(s.context) = UPPER(sche.target_type) AND s.target_id = sche.target_id)
+    """
+).createOrReplaceTempView("search_engine")
+
+spark.sql(
+    """
+    SELECT
+        transaction_id
+        , target_type
+        , target_id
+        , context
+        , punctuality
+        , session_status
+        , COUNT(DISTINCT schedule_id) AS schedule_count
+        , COUNT(DISTINCT session_id) AS session_count
+        , MIN(session_created_at) AS first_session_created_at
+        , MAX(session_created_at) AS last_session_created_at
+    FROM search_engine
+    GROUP BY transaction_id, target_type, target_id, context, punctuality, session_status
+    """
+).createOrReplaceTempView("search_engine_facts")
+
+spark.sql("SELECT * FROM search_engine").write.mode("overwrite").parquet("data/search_engine")
+spark.sql("SELECT * FROM search_engine_facts").write.mode("overwrite").parquet("data/search_efficiency_facts")
